6c9a3ec72ceb05373b02fb43a1385228380ea93b
dust1nmax <dust1nmax0712@gmail.com>
2026-09-14 19:13:32 +0800

    demo添加独立go.mod

---
+++ demos/04_embedder/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"context"
+
+	"github.com/joho/godotenv"
+	
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
+
+	ctx := context.Background()
+	
+	embedder, err := 
+
+	
+}
